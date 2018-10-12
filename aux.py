be62826893915a722b4b8d3611a7640e8d85278d
Rich Pang <rpang@uw.edu>
2018-10-12 16:57:53 -0700

    Add class for storing temp vars.

--- aux.py
+++ aux.py
@@ -2,10 +2,26 @@ import numpy as np
 import os
 
 
-class GenericFlexible(object):
+class Generic(object):
     """Class for generic object."""
     
     def __init__(self, **kwargs):
         
         for k, v in kwargs.items():
             self.__dict__[k] = v
+            
+
+class TempContainer(object):
+    """
+    Class to contain temporary variables.
+    
+    Usage:
+    >>> TMP = TempContainer()
+    >>> TMP.num = (3 + 1 + 5 + 6 + 3)
+    >>> TMP.denom = (2 + 4 + 3 + 2)
+    >>> x = TMP.num / TMP.denom
+    >>> TMP.reset()
+    """
+    
+    def reset(self):
+        self.__dict__ = {}
