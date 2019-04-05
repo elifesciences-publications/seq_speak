ab042fae6da0eb77f641b592ca741546ba22e7ed
Rich Pang <rpang@uw.edu>
2019-04-05 12:40:05 -0700

    Code up event-detection function.

--- aux.py
+++ aux.py
@@ -45,3 +45,23 @@ def lognormal_mu_sig(mean, std):
     sig = np.sqrt(np.log(b))
     
     return mu, sig
+
+
+# DATA PROCESSING
+def get_segments(x, t=None):
+    """
+    Return the numerical indices indicating the segments of non-False x-values.
+    :param x: boolean time-series
+    :param t: vector containing indices to use if not 0 to len(x)
+    :return: starts, ends, which are numpy arrays containing the start and end idxs of
+        segments of consecutive Trues (end idxs are according to Python convention, e.g.,
+        np.array([False, False, False, True, True, False]) yields (array([3]), array([5]))
+    """
+
+    if t is None:
+        t = np.arange(len(x), dtype=int)
+
+    starts = t[(np.diff(cc([[False], x]).astype(int)) == 1).nonzero()[0]]
+    ends = t[(np.diff(cc([x, [False]]).astype(int)) == -1).nonzero()[0]] + 1
+
+    return starts, ends
